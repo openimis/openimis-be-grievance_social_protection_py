ed183dddd5ad8c61079e33c42d6136b5b36ce0e6
delcroip <patrick.delcroix@swisstph.ch>
2025-12-05 10:20:24 +0100

    refactor(test): update grievance tests to use create_test_grievance_user instead of create_test_interactive_user
    Replace create_test_interactive_user with create_test_grievance_user in setUpClass for gql_ticket_comment_create, gql_ticket_create, gql_ticket_reopen, and gql_ticket_resolve_by_comment tests. This aligns test user creation with grievance-specific helpers, improving consistency and modularity in the grievance_social_protection module tests.

--- grievance_social_protection/tests/test_gql_ticket_create.py
+++ grievance_social_protection/tests/test_gql_ticket_create.py
@@ -2,10 +2,10 @@ from django.test import TestCase
 from core.models import MutationLog
 from graphene import Schema
 from graphene.test import Client
-from core.test_helpers import create_test_interactive_user
 from grievance_social_protection.models import Ticket
 from grievance_social_protection.schema import Query, Mutation
 from grievance_social_protection.tests.gql_payloads import gql_mutation_create_ticket
+from grievance_social_protection.tests.test_helpers import create_test_grievance_user
 from core.models.openimis_graphql_test_case import openIMISGraphQLTestCase, BaseTestContext
 
 
@@ -25,7 +25,7 @@ class GQLTicketCreateTestCase(openIMISGraphQLTestCase):
     @classmethod
     def setUpClass(cls):
         super(GQLTicketCreateTestCase, cls).setUpClass()
-        cls.user = create_test_interactive_user(username='user_authorized', roles=[7])
+        cls.user = create_test_grievance_user(username='user_authorized')
 
         gql_schema = Schema(
             query=Query,
